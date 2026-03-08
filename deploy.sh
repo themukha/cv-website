6e43c4b0f22279961d9d1cee6f69d5c4857cebdc
Georgii Mukha <george@themukha.tech>
2026-03-08 16:01:59 +0100

    Migrate Docker deployment to GitHub Actions with updated workflow and clean up obsolete scripts.

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,9 @@
 #!/bin/bash
 
-cd /opt/cv-website
+echo "$(date): Starting deploy..."
 
-# 1. Get updates from repo
-git fetch origin master
-
-# 2. Get hashes of last local and remote commits
-LOCAL_HASH=$(git rev-parse HEAD)
-REMOTE_HASH=$(git rev-parse origin/master)
-
-# 3. Compare them
-if [ "$LOCAL_HASH" = "$REMOTE_HASH" ]; then
-    echo "$(date): No changes detected. Skipping build."
-    # 3.1. Stop script if no changes
-    exit 0
-fi
-
-echo "$(date): Changes detected! Starting deploy..."
-
-# 4. If hashes are different - update repo
-git reset --hard origin/master
-
-# 5. Rebuild
 docker-compose up -d --build
 
-# 6. Clean
 docker image prune -f
 
 echo "$(date): Deploy finished successfully."
